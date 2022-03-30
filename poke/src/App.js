5b7e4ac4f20da124aebf8c0d802590d82064cb5f
Mario Granados <mario.alberto.granados1@gmail.com>
2022-03-30 01:28:28 -0500

    added api request and local storage

---
+++ poke/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import {useState} from "react";
+
+function App() {
+    let url = 'https://pokeapi.co/api/v2/pokemon';
+    let request = () => {
+        fetch(url)
+            .then((response) => response.json())
+            .then((json) => {
+                pokemon.push(json);
+                window.localStorage.setItem("pokemon", JSON.stringify(pokemon));
+            });
+    }
+    let pokemon = [];
+    if(JSON.parse(window.localStorage.getItem('pokemon')) !== null) {
+        pokemon = JSON.parse(window.localStorage.getItem('pokemon'));
+        console.log("from local")
+        console.log(pokemon)
+    } else {
+        console.log("new Request")
+        request();
+        console.log(pokemon)
+    }
+
+    return (
+        <>
+
+        </>
+    );
+}
+
+export default App;
